0e0ad7af9f83bd524c809b7bce7aea4d7ac71637
Finn Masters <finnjmasters@gmail.com>
2023-03-20 14:58:39 +1300

    Updated Gambling Command.py

--- Tests/Gambling Command.py
+++ Tests/Gambling Command.py
@@ -13,9 +13,18 @@ while StillGambling == True:
     chance = randint(1,10)
     if GambleAmount.isdigit():
         GambleAmount = int(GambleAmount)
-        if chance == randint(1,10):
-            print("You Won", GambleAmount * 2)
-        else:
+        if GambleAmount < money and chance == 6:
+            print("You Won", GambleAmount * 2,"!")
+            print(Fore.GREEN + "You now Have $", GambleAmount + money)
+            print(Fore.GREEN)
+        elif GambleAmount > money:
+            print(Fore.RED + "You don't have enough money for that.")
+            print("Fore.WHITE")
+        elif GambleAmount < money and chance != 6:
             print("You lost", GambleAmount)
+            print(Fore.GREEN + "You now Have $", money - GambleAmount)
+            print(Fore.WHITE)
+        else:
+            print("Sorry, Please Try Again")
     else:
         print("Please enter a whole number next time.")
