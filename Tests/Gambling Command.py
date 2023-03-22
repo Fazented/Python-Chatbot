48cf4b6da12542231ee60c4d877c3fef0cea74f5
Finn Masters <finnjmasters@gmail.com>
2023-03-23 09:29:46 +1300

    fixed the issues in gambling command.py

--- Tests/Gambling Command.py
+++ Tests/Gambling Command.py
@@ -6,24 +6,27 @@ money = 1000
 StillGambling = True
 
 print(Fore.WHITE)
-print(Fore.GREEN + "You Have $", money)
+print(Fore.GREEN + f"You Have ${money}")
 print(Fore.WHITE)
 
 while StillGambling == True:
-    GambleAmount = input("How much money do you want to gamble? ")
-    chance = randint(1,10)
+    GambleAmount = input(Fore.WHITE + "How much money do you want to gamble? ")
+    chance = randint(1,6)
     if GambleAmount.isdigit():
         GambleAmount = int(GambleAmount)
-        if GambleAmount < money and chance == 6:
-            print("You Won", GambleAmount * 2,"!")
-            print(Fore.GREEN + "You now Have $", GambleAmount + money)
+        if GambleAmount <= money and chance == 6:
+            GambleAmount*=2
+            print(f"You Won ${GambleAmount}")
+            GambleAmount += money
+            print(Fore.GREEN + f"You now Have ${money}")
             print(Fore.WHITE)
         elif GambleAmount > money:
             print(Fore.RED + "You don't have enough money for that.")
             print(Fore.WHITE)
-        elif GambleAmount < money and chance != 6:
-            print("You lost", GambleAmount)
-            print(Fore.GREEN + "You now Have $", money - GambleAmount)
+        elif GambleAmount <= money and chance != 6:
+            print(f"You lost ${GambleAmount}!")
+            money -= GambleAmount
+            print(Fore.GREEN + f"You now Have ${money}")
             print(Fore.WHITE)
         else:
             print("Sorry, Please Try Again")
