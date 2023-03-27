aabe232fe7189f979b35662c0caf64153f93904f
Finn Masters <finnjmasters@gmail.com>
2023-03-28 09:44:56 +1300

    Made Quote command display image

--- Tests/Inspirational Quote.py
+++ Tests/Inspirational Quote.py
@@ -1,11 +1,15 @@
-
 import inspirobot
-
+from PIL import Image
+import requests
 
 choice = input("What do you want me to do? ").lower()
 
 if "quote" in choice :
     quote = inspirobot.generate()
-    print(quote.url) # I am working on a way to display this image 
+    #print(quote.url)
+    img_url = quote.url
+    img = Image.open(requests.get(img_url, stream = True).raw)
+    img.save('tests\inspiration.jpg')
+    img.show()
 else:
-    print("error")
+    print("Error")
