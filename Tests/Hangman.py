a62da4222d01eabc8a38ea3488b27f1976865bbf
Finn Masters <finnjmasters@gmail.com>
2023-04-04 12:06:40 +1200

    fixed hangman

--- Tests/Hangman.py
+++ Tests/Hangman.py
@@ -1,31 +1,36 @@
 import random
 
 words = ["python", "computer", "programming", "code", "algorithm"]
-word = random.choice(words)
 
-guesses = ""
-turns = 10
+choice = input("What do you want me to do? ").lower
 
-while turns > 0:
-    failedwords = 0
-    for char in word:
-        if char in guesses:
-            print(char, end="")
-        else:
-            print("_", end="")
-            failedwords += 1
+if "hangman" in choice:
+    guesses = ""
+    turns = 10
+    word = random.choice(words)
 
-    if failedwords == 0:
-        print("")
-        print("You won the game!")
-        break
-    guess = input("\n\nGuess a character: ")
-    guesses += guess
+    while turns > 0:
+        failedwords = 0
+        for char in word:
+            if char in guesses:
+                print(char, end="")
+            else:
+                print("_", end="")
+                failedwords += 1
 
-    if guess not in word:
-        turns -= 1
-        print("Incorrect")
-        print(f"You have {turns} turns left.")
-        if turns == 0:
-            print("You lost the game.")
-            print("The word was", word)
+        if failedwords == 0:
+            print("")
+            print("You won the game!")
+            break
+        guess = input("\n\nGuess a character: ")
+        guesses += guess
+
+        if guess not in word:
+            turns -= 1
+            print("Incorrect")
+            print(f"You have {turns} turns left.")
+            if turns == 0:
+                print("You lost the game.")
+                print("The word was", word)
+else:
+    print("Error")
