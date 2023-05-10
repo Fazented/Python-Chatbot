700c12557ea1106c4a63b1b64b0d2406b443e206
Finn Masters <finnjmasters@gmail.com>
2023-05-11 10:00:45 +1200

    Updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+from random import randint #importing all the packages
+import time
+import inspirobot
+from PIL import Image
+import requests
+import python_weather
+import asyncio
+import os
+import datetime
+
+commandlist = ["Joke", "Flip a Coin", "Weather", "Gambling", "Hangman", "Inspirational Quote", "Help" ] # List of commands for help command
+words = ["python", "computer", "programming", "code", "algorithm"] #words to guess in Hangman
+
+print("Hello, I am Sadness Bot. An eternally sad chatbot. Use 'Help' to see a list of commands.")
+choice = input("What do you want me to do? ")
+
+
+
+
+
+
+
+
+
